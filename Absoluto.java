9cb87ff7640303a7d5fed6b317cf05cbd3726e4f
Helissom <51801139+Helissom@users.noreply.github.com>
2022-06-16 00:04:15 -0700

    Absoluto.java
    Your task in this problem is to implement a computer program that receives a
    integer and show its absolute value, i.e. its modulus.

---
+++ Absoluto.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.math.*;
+public class Absoluto
+{
+  public static void main(String[] args)
+  {
+  Scanner sc;
+    sc = new Scanner(System.in);
+    long x = sc.nextLong();
+    System.out.println(Math.abs(x));
+    
+  }
+}
