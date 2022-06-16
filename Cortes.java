202c63b844969bb0a51f598f69332d62cf1066b0
Helissom <51801139+Helissom@users.noreply.github.com>
2022-06-16 00:08:16 -0700

    Update Cortes.java

--- Cortes.java
+++ Cortes.java
@@ -5,7 +5,7 @@ public class Cortes {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 		System.out.println();
-     	int c=scan.nextInt();
+     	int c = scan.nextInt();
 		System.out.println();
         System.out.println((c * (c + 1) >> 1) + 1);
     }
