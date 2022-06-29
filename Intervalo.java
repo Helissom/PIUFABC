78361df10763ab3ee283613dfba29a02e32f6334
Helissom <51801139+Helissom@users.noreply.github.com>
2022-06-28 22:29:55 -0700

    Faça um programa que leia um número qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Se o valor não estiver em nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.

---
+++ Intervalo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Intervalo
+{
+public static void main(String[] args)
+  {
+    double numqualquer;
+    Scanner scan = new Scanner(System.in);
+    numqualquer = scan.nextDouble();
+
+    if (numqualquer >=0 && numqualquer <= 25)
+      System.out.println("intervalo [0,25]");
+    else if (numqualquer > 25 && numqualquer < 50)
+      System.out.println("intervalo (25,50]");
+    else if (numqualquer >= 50 && numqualquer < 75)
+      System.out.println("intervalo (50,75]");
+    else if (numqualquer >= 75 && numqualquer <= 100)
+      System.out.println("intervalo (75,100]");
+    else{
+      System.out.println("Fora de intervalo");
+    }
+  }    
+}
